66169e5dddec9a2ba1ce2beb763fb205bd5dd1a7
EkowYamoah <42788503+EkowYamoah@users.noreply.github.com>
2018-09-13 03:29:46 +0000

    Add files via upload

---
+++ a4_10678514.cc
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+using namespace std;
+
+int gcd(int Fnum, int Snum);
+
+
+int main() {
+	int Fnum, Snum, C;
+/*This program calculates the Greatest common divisor of to numbers*/	
+	cout<<"PLEASE ENTER FIRST NUMBER HERE: ";
+	cin>>Fnum;
+	cout<<"PLEASE ENTER SECOND NUMBER:  ";
+	cin>>Snum;
+		
+	cout<<"THE GCD OF "<< Fnum<<" AND "<<Snum <<" IS: " << gcd(Fnum, Snum);	
+	
+
+	return 0;
+}
+	int gcd(int Fnum, int Snum){
+		
+		if(Snum != 0)
+		return  gcd(Snum, Fnum % Snum);
+		else
+			return Fnum;
+}
